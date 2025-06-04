c31295f876dcba52452b9d8afda580d1dc887049
clement Roland <clementroland52@gmail.com>
2025-06-03 21:58:35 -0400

    mettre dans un tableau afin d'utiliser la boucle for pour afficher tous les données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,38 @@ import { FaceSnap } from "./models/face-snap";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  mySnap!: FaceSnap;
-  midwaySnap!: FaceSnap;
-  oldSnap!: FaceSnap;
+  allSnap!: FaceSnap[];
+  
   ngOnInit():void{
-    this.mySnap = new FaceSnap(
+    this.allSnap = [
+
+      new FaceSnap(
       'Archibald',
       'Mon meilleur ami depuis tout petit !',
       new Date(),
       6,
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
     ),
-    this.midwaySnap=  new FaceSnap(
+      
+      new FaceSnap(
       'new-York',
       'new york city on tne night',
       new Date(),
       130,
       'https://images.pexels.com/photos/32369819/pexels-photo-32369819/free-photo-of-sentiers-de-feux-de-circulation-nocturnes-dynamiques-de-la-ville-de-new-york.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     ),
-    this.oldSnap=  new FaceSnap(
+    new FaceSnap(
       'miami for work',
       'i love miami',
       new Date(),
       100,
       'https://images.pexels.com/photos/4118101/pexels-photo-4118101.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     )
-    this.midwaySnap.setLocation('new-york');
-    this.oldSnap.setLocation('miami');
+    ];
+    
+    
+    
+    this.allSnap[1].setLocation('new-york');
+    this.allSnap[2].setLocation('miami');
   }
 }
