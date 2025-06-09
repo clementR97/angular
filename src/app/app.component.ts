e19a1fddce19c33b9bac1ba07e813912f09b94fb
clement Roland <clementroland52@gmail.com>
2025-06-09 12:40:23 -0400

    Refactorisation de l'affichage des FaceSnaps : remplacement de la boucle dans app.component.html par un composant FaceSnapList. Ajout de la logique d'initialisation des données dans face-snap-list.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FaceSnapComponent } from "./face-snap/face-snap.component";
 import { FaceSnap } from "./models/face-snap";
+import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FaceSnapComponent],
+  imports: [FaceSnapComponent,FaceSnapListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { FaceSnapComponent } from '../face-snap/face-snap.component';
 
 @Component({
   selector: 'app-face-snap-list',
-  imports: [],
+  imports: [FaceSnapComponent],
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
 export class FaceSnapListComponent {
-
+  allSnap!: FaceSnap[];
+  ngOnInit():void{
+    this.allSnap = [
+      
+    ]
+  }
 }
